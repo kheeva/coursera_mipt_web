65aea3efcc18a77bb77b83d2aefa0a99f8d5f4f5
kheeva <kheeva@ya.ru>
2019-03-12 20:34:14 +0300

    bs4 in progress

--- bs4/soup_sample/wikistat4.py
+++ bs4/soup_sample/wikistat4.py
@@ -79,8 +79,17 @@ def parse(start, end, path):
             if re.match(r'^[ETC].*$', h_tag.text):
                 headers += 1
 
+        links_seq_len = 0
+        max_links_seq_len = 0
+        tags = [elem.name for elem in body.find('a').next_elements]
 
-
+        for tag in tags:
+            # print('tag', tag.name)
+            if tag == 'a':
+                links_seq_len += 1
+            elif tag is not None:
+                max_links_seq_len = max(links_seq_len, max_links_seq_len)
+                links_seq_len = 0
 
         # TODO посчитать реальные значения
         # imgs = 5  # Количество картинок (img) с шириной (width) не меньше 200
@@ -88,26 +97,82 @@ def parse(start, end, path):
         # linkslen = 15  # Длина максимальной последовательности ссылок, между которыми нет других тегов
         # lists = 20  # Количество списков, не вложенных в другие списки
 
-        out[file] = [images_count, headers]
+        out[file] = [images_count, headers, max_links_seq_len]
 
     return out
 
+
 start = 'Stone_Age'
+# start = 'Brain'
 end = 'Python_(programming_language)'
 path = './wiki/'
-
-def lll(file, path):
-    with open("{}{}".format(path, file)) as data:
-        soup = BeautifulSoup(data, "lxml")
-
-    body = soup.find(id="bodyContent")
-
-    # print(type(body.contents))
-    print(re.findall(r'(?:<a.*?</a>)(?:[^<a.*?</a>])(?:<a.*?</a>)', str(body.contents)))
-
-lll(start, path)
 
 # t = time.time()
 # x = parse(start, end, path)
 # print(x)
 # print(time.time() - t)
+
+with open("{}{}".format(path, 'Brain')) as data:
+    soup = BeautifulSoup(data, "lxml")
+
+# body = soup.find('div', {'id': 'bodyContent'})
+body = soup.find(id="bodyContent")
+print(dir(body))
+
+links_seq_len = 0
+max_links_seq_len = 0
+tags = [elem.name for elem in body.next_elements if elem.name is not None]
+print(tags)
+print(soup.tagStack)
+for tag in tags:
+    if tag in ['a',]:
+        links_seq_len += 1
+    else:
+        max_links_seq_len = max(links_seq_len, max_links_seq_len)
+        links_seq_len = 0
+
+print(max_links_seq_len)
+
+#
+# def lll(file, path):
+#     # with open("{}{}".format(path, file)) as data:
+#     #     soup = BeautifulSoup(data, "lxml")
+#     #
+#     # body = soup.find(id="bodyContent")
+#
+#     regex = r"(?:<a.*?</a>)+[^(<a)]*?(?:<a.*?</a>)+"
+#
+#     test_str = ("<a href=\"Stone age\">Stone age</a>\n"
+#                 "<p>paragraph</p>\n"
+#                 "<a href=\"Python\">Python</a> текст <a href=\"C\">C</a>\n"
+#                 "<a href=\"Javascript\">Javascript</a>\n"
+#                 "<b>text</b><br />\n"
+#                 "<a href='Scala'>Scala</a> <a href=\"Scala2\">Scala2</a>")
+#
+#     # print(type(body.contents))
+#     print(re.findall(regex, test_str))
+
+# lll(start, path)
+# test_str = ("<a href=\"Stone age\">Stone age</a>\n"
+#                 "<p>paragraph</p>\n"
+#                 "<a href=\"Python\">Python</a> текст <a href=\"C\">C</a>\n"
+#                 "<a href=\"Javascript\">Javascript</a>\n"
+#                 "<b>text</b><br />\n"
+#                 "<a href='Scala'>Scala</a> <a href=\"Scala2\">Scala2</a>")
+#
+# b = BeautifulSoup(test_str, 'lxml')
+
+
+# links_count = 0
+# max_sequence = 0
+#
+# for i in b.body.next_elements:
+#     if i.name == 'a':
+#         links_count += 1
+#         print(i.string, links_count)
+#     elif i.name is not None:
+#         max_sequence = max(links_count, max_sequence)
+#         links_count = 0
+#
+#
+# print(max_sequence)
