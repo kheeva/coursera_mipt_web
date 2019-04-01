e8374f2f175ef79c94f34ded501cbe1df2a13dc1
kheeva <kheeva@ya.ru>
2019-04-01 19:54:21 +0300

    post to controller in progress

--- smart_house/coursera_house/core/tasks.py
+++ smart_house/coursera_house/core/tasks.py
@@ -1,9 +1,87 @@
 from __future__ import absolute_import, unicode_literals
 from celery import task
 
+import requests
+import json
+from django.conf import settings
 from .models import Setting
+
+
+def get_sensors_data():
+    auth_header = {'Authorization': f'Bearer {settings.SMART_HOME_ACCESS_TOKEN}'}
+    user_request = requests.get(settings.SMART_HOME_API_URL, headers=auth_header)
+    return user_request.json()
+
+
+def update_sensors_data(data):
+    data_to_send = {"controllers": []}
+    for key, value in data.items():
+        data_to_send['controllers'].append(
+            {"name": key, "value": value}
+        )
+    auth_header = {'Authorization': f'Bearer {settings.SMART_HOME_ACCESS_TOKEN}'}
+    r = requests.post(settings.SMART_HOME_API_URL,
+                  headers=auth_header,
+                  data=json.dumps(data_to_send))
+    print('xxx')
+    print(json.dumps(data_to_send))
 
 @task()
 def smart_home_manager():
-    # Здесь ваш код для проверки условий
-    pass
+    sensors_raw_data = get_sensors_data()
+    sensors_dict = {}
+    updated_sensors_dict = {}
+
+    bedroom_target_temperature = Setting.objects.get(
+        label='bedroom_target_temperature').value
+
+    hot_water_target_temperature = Setting.objects.get(
+        label='hot_water_target_temperature').value
+
+
+    for sensor in sensors_raw_data['data']:
+        sensors_dict[sensor['name']] = sensor['value']
+
+    if sensors_dict['leak_detector']:
+        updated_sensors_dict['cold_water'] = False
+        updated_sensors_dict['hot_water'] = False
+        updated_sensors_dict['boiler'] = False
+        updated_sensors_dict['washing_machine'] = False
+
+    if not sensors_dict['cold_water']:
+        updated_sensors_dict['boiler'] = False
+        updated_sensors_dict['washing_machine'] = False
+
+    if sensors_dict['cold_water'] and not sensors_dict['smoke_detector']:
+        hot_water_koef = sensors_dict['boiler_temperature']/(
+            hot_water_target_temperature)
+
+        if round((hot_water_koef-1)*100) <= -10:
+            updated_sensors_dict['boiler'] = True
+
+        if round((hot_water_koef-1)*100) >= 10:
+            updated_sensors_dict['boiler'] = False
+
+    if not sensors_dict['curtains'] == 'slightly_open':
+        if sensors_dict['outdoor_light'] < 50 and not sensors_dict['bedroom_light']:
+            updated_sensors_dict['curtains'] = 'open'
+
+        if sensors_dict['outdoor_light'] > 50 or sensors_dict['bedroom_light']:
+            updated_sensors_dict['curtains'] = 'close'
+
+    if sensors_dict['smoke_detector']:
+        for device in ['air_conditioner', 'bedroom_light', 'bathroom_light',
+                       'boiler', 'washing_machine']:
+            updated_sensors_dict[device] = False
+
+    if not sensors_dict['smoke_detector']:
+        temp_koef = sensors_dict['bedroom_temperature'] / (
+            bedroom_target_temperature)
+
+        if round((temp_koef-1)*100) >= 10:
+            sensors_dict['air_conditioner'] = True
+
+        if round((temp_koef-1)*100) <= -10:
+            sensors_dict['air_conditioner'] = False
+
+    update_sensors_data(updated_sensors_dict)
--- smart_house/coursera_house/core/views.py
+++ smart_house/coursera_house/core/views.py
@@ -3,6 +3,7 @@ from django.views.generic import FormView
 
 from .models import Setting
 from .form import ControllerForm
+from .tasks import get_sensors_data
 
 # to delete
 # from django.utils.decorators import method_decorator
@@ -15,43 +16,53 @@ class ControllerView(FormView):
     form_class = ControllerForm
     template_name = 'core/control.html'
     success_url = reverse_lazy('form')
+    bed_label = 'bedroom_target_temperature'
+    bath_label = 'hot_water_target_temperature'
 
     def get_context_data(self, **kwargs):
         context = super(ControllerView, self).get_context_data()
-        context['data'] = {'test': 'test'}
+        sensors_data = get_sensors_data()['data']
+        context['data'] = {}
+        for sensor in sensors_data:
+            context['data'][sensor['name']] = sensor['value']
         return context
 
     def get_initial(self):
         initial = super(ControllerView, self).get_initial()
-        bed_label = 'bedroom_target_temperature'
-        initial[bed_label] = Setting.objects.get(label=bed_label).value
-        bath_label = 'hot_water_target_temperature'
-        initial[bath_label] = Setting.objects.get(label=bath_label).value
+
+        if not Setting.objects.filter(label=self.bed_label):
+            bedroom_setting = Setting(
+                controller_name='Oleg Ivliev \'s smart house',
+                label=self.bed_label,
+                value=21)
+            bedroom_setting.save()
+
+        if not Setting.objects.filter(label=self.bath_label):
+            bathroom_setting = Setting(
+                controller_name='Oleg Ivliev \'s smart house',
+                label=self.bath_label,
+                value=80)
+            bathroom_setting.save()
+
+        initial[self.bed_label] = Setting.objects.get(
+            label=self.bed_label).value
+        initial[self.bath_label] = Setting.objects.get(
+            label=self.bath_label).value
+
         return initial
 
     def update_settings(self, form):
-        bed_label = 'bedroom_target_temperature'
-        stored_bedroom_temp = Setting.objects.get(label=bed_label).value
-        form_bedroom_temp = int(form.data[bed_label])
-        if stored_bedroom_temp != form_bedroom_temp:
-            print(
-                f'stored - {stored_bedroom_temp} \n from form - {form_bedroom_temp}')
-            Setting.objects.filter(label=bed_label).update(
-                value=form_bedroom_temp)
+        initial_data = form.initial
+        posted_data = form.cleaned_data
+        if posted_data[self.bed_label] != initial_data[self.bed_label]:
+            Setting.objects.filter(label=self.bed_label).update(
+                value=posted_data[self.bed_label])
 
-        bath_label = 'hot_water_target_temperature'
-        stored_bathroom_temp = Setting.objects.get(label=bath_label).value
-        form_bathroom_temp = int(form.data[bath_label])
-        if stored_bathroom_temp != form_bathroom_temp:
-            print(
-                f'stored - {stored_bathroom_temp} \n from form - {form_bathroom_temp}')
-            Setting.objects.filter(label=bath_label).update(
-                value=form_bathroom_temp)
+        if posted_data[self.bath_label] != initial_data[self.bath_label]:
+            Setting.objects.filter(label=self.bath_label).update(
+                value=posted_data[self.bath_label])
+
 
     def form_valid(self, form):
-        print(form.cleaned_data)
-        print(form.changed_data)
-        print(form.initial)
-        # print(dir(form))
-        # self.update_settings(form)
+        self.update_settings(form)
         return super(ControllerView, self).form_valid(form)
